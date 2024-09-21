03a87514fb28ca8a8dfdfe1b82e967aa9f72cbe1
Yuvraj <devyuvraj883@gmail.com>
2024-09-22 05:16:26 +0530

    Added API request to the dummyJson

--- e-commerce/main.js
+++ e-commerce/main.js
@@ -1,1 +1,21 @@
-import './style.css'
+
+const URL = 'https://dummyjson.com/products';
+
+// const products = await fetch(URL);
+
+let products;
+
+fetch(URL).then((res)=>{
+  // console.log(res);
+  return res.json();
+})
+.then((data)=>{
+  products = data; 
+  console.log(data)})
+.catch((e)=>{
+  console.log(e);
+})
+
+// fetch('https://dummyjson.com/products')
+// .then(res => res.json())
+// .then(console.log);
