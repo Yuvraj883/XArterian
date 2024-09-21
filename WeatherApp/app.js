f1c35e7d678fa9434c61138bb5ccf5caf6e697fe
Yuvraj <devyuvraj883@gmail.com>
2024-09-21 12:43:59 +0530

    Connected with the html page

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,17 +1,29 @@
-// alert("Jai Shri Ram");
-const API_KEY = 'b99b652d448e47e5a48a226c5ed84910'
+const searchIcon = document.getElementById('search-icon');
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${'delhi'}&appid=${API_KEY}`
+const getWeatherInfo = ()=>{
+  const API_KEY = 'b99b652d448e47e5a48a226c5ed84910';
+  const city = document.getElementById('city-input').value;
 
-fetch(url).then((res)=>{
-  if(!res.ok){
-    console.log( `API Call Failed with status ${res.status}`);
-  }
-  else{
-    return res.json();
-  }
-}).then((data)=>{
-  console.log(data?.main);
-}).catch(error=>{
-  console.log(error);
-})
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  fetch(url).then((res)=>{
+    if(!res.ok){
+      console.log( `API Call Failed with status ${res.status}`);
+    }
+    else{
+      return res.json();
+    }
+  }).then((data)=>{
+    document.querySelector('.output').classList.add('visible');
+    document.querySelector('.city-name').textContent = `${data?.name}`
+    document.querySelector('.temperature').textContent = `Temp☀️: ${(data?.main?.temp/10).toFixed(1)}°C`
+    document.querySelector('.humidity').textContent = `Humidity☁️: ${data?.main?.humidity}%`
+
+    console.log(data?.main);
+  }).catch(error=>{
+    console.log(error);
+  })
+}
+
+
+searchIcon.addEventListener('click', getWeatherInfo);
+
