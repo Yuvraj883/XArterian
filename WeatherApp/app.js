215c0089928d902b83353b609679c414dfdb6d03
Yuvraj <devyuvraj883@gmail.com>
2024-09-21 17:09:08 +0530

    Added search on key down(Enter) feature

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,45 +1,49 @@
-const searchIcon = document.getElementById('search-icon');
-let inputField = document.getElementById('city-input');
+const searchIcon = document.getElementById('search-icon')
+let inputField = document.getElementById('city-input')
 
-
-const getWeatherInfo = ()=>{
-  const API_KEY = 'b99b652d448e47e5a48a226c5ed84910';
-  let city = inputField.value;
-  if(city===''){
-    city='delhi';
+const getWeatherInfo = () => {
+  const API_KEY = 'b99b652d448e47e5a48a226c5ed84910'
+  let city = inputField.value
+  if (city === '') {
+    city = 'delhi'
   }
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-  fetch(url).then((res)=>{
-    if(!res.ok){
-      console.log( `API Call Failed with status ${res.status}`);
-    }
-    else{
-      return res.json();
-    }
-  }).then((data)=>{
-    document.querySelector('.output').classList.add('visible');
-    document.querySelector('.city-name').textContent = `${data?.name}📍`;
-    document.querySelector('.temperature').textContent = `Temp☀️: ${(data?.main?.temp/10).toFixed(1)}°C`;
-    document.querySelector('.humidity').textContent = `Humidity☁️: ${data?.main?.humidity}%`;
-    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`;
-    document.querySelector('.description').textContent = `${data?.weather[0]?.description}`;
-    document.querySelector('.heading').textContent='';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        console.log(`API Call Failed with status ${res.status}`)
+      } else {
+        return res.json()
+      }
+    })
+    .then((data) => {
+      document.querySelector('.output').classList.add('visible')
+      document.querySelector('.city-name').textContent = `${data?.name}📍`
+      document.querySelector('.temperature').textContent = `Temp☀️: ${(
+        data?.main?.temp / 10
+      ).toFixed(1)}°C`
+      document.querySelector(
+        '.humidity'
+      ).textContent = `Humidity☁️: ${data?.main?.humidity}%`
+      document.querySelector(
+        '.icon'
+      ).src = `https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`
+      document.querySelector(
+        '.description'
+      ).textContent = `${data?.weather[0]?.description}`
+      document.querySelector('.heading').textContent = ''
 
-
-
-
-    // console.log(data?.weather[0]);
-  }).catch(error=>{
-    console.log(error);
-  })
+      
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
-
-searchIcon.addEventListener('click', getWeatherInfo);
-inputField.addEventListener('keydown', (event)=>{
-  if(event.key==='Enter'){
-    getWeatherInfo();
+searchIcon.addEventListener('click', getWeatherInfo)
+inputField.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    getWeatherInfo()
   }
 })
-
