1fd03a8314a49d0391e6d768b3048354d183994f
Yuvraj <devyuvraj883@gmail.com>
2024-09-22 05:45:56 +0530

    Added dynamic rendering of products

--- e-commerce/main.js
+++ e-commerce/main.js
@@ -1,21 +1,27 @@
-
 const URL = 'https://dummyjson.com/products';
-
-// const products = await fetch(URL);
 
 let products;
 
-fetch(URL).then((res)=>{
-  // console.log(res);
-  return res.json();
-})
-.then((data)=>{
-  products = data; 
-  console.log(data)})
-.catch((e)=>{
-  console.log(e);
-})
+fetch(URL)
+  .then((res) => res.json())
+  .then((data) => {
+    products = data.products;
+    const template = document.querySelector('#product-card');
+    const productList = document.querySelector('#product-list');
+    products.forEach(product => {
+      const productNode = template.content.cloneNode(true);
+      productNode.querySelector('#product-title').textContent = product?.name || 'N/A';
+      productNode.querySelector('#product-description').textContent = product?.description || 'No description available';
 
-// fetch('https://dummyjson.com/products')
-// .then(res => res.json())
-// .then(console.log);
+      const discountedPrice = product.price * (1 - product.discountPercentage / 100);
+      productNode.querySelector('#product-price').textContent = discountedPrice.toFixed(2);
+      productNode.querySelector('#product-actual-price').textContent = product?.price.toFixed(2);
+      productNode.querySelector('#product-img').src = product?.images[0] || 'default-image.jpg';
+      productNode.querySelector('#product-rating').textContent = `${product.rating} ⭐`;
+
+      productList.append(productNode);
+    });
+  })
+  .catch((e) => {
+    console.error('Error fetching products:', e);
+  });
