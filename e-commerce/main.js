133f2976e9cc69821b4010e89bc6bb588f30fbea
Yuvraj <devyuvraj883@gmail.com>
2024-09-22 07:14:31 +0530

    Updated product details

--- e-commerce/main.js
+++ e-commerce/main.js
@@ -1,4 +1,4 @@
-const URL = 'https://dummyjson.com/products';
+const URL = 'https://dummyjson.com/products/?limit=40';
 
 let products;
 
@@ -6,18 +6,30 @@ fetch(URL)
   .then((res) => res.json())
   .then((data) => {
     products = data.products;
+    console.log(products);
     const template = document.querySelector('#product-card');
     const productList = document.querySelector('#product-list');
     products.forEach(product => {
       const productNode = template.content.cloneNode(true);
-      productNode.querySelector('#product-title').textContent = product?.name || 'N/A';
+      productNode.querySelector('#product-title').textContent = product?.title || 'N/A';
       productNode.querySelector('#product-description').textContent = product?.description || 'No description available';
-
+      productNode.querySelector('#product-brand').textContent = product?.brand;
       const discountedPrice = product.price * (1 - product.discountPercentage / 100);
-      productNode.querySelector('#product-price').textContent = discountedPrice.toFixed(2);
-      productNode.querySelector('#product-actual-price').textContent = product?.price.toFixed(2);
-      productNode.querySelector('#product-img').src = product?.images[0] || 'default-image.jpg';
+      productNode.querySelector('#product-price').textContent = `$ ${discountedPrice.toFixed(2)}`;
+      productNode.querySelector('#product-actual-price').textContent = `$${product?.price.toFixed(2)}`;
+      productNode.querySelector('#product-img').src = product?.thumbnail || 'default-image.jpg';
       productNode.querySelector('#product-rating').textContent = `${product.rating} ⭐`;
+      productNode.querySelector('#product-page-link').href=`product.html?id=${product?.id}`
+
+      const tagsContainer = productNode.querySelector('#product-tags');
+
+      product?.tags.forEach(tag=>{
+        const tagElement = document.createElement('p');
+        tagElement.textContent = tag;
+        tagElement.classList.add('border-2', 'px-2', 'py-1', 'border-black', 'm-1', 'rounded-md' );
+        tagsContainer.appendChild(tagElement);
+
+      })
 
       productList.append(productNode);
     });
