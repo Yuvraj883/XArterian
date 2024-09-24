6ca1994070bff0614a3553aadc1b7ec08b39599a
Yuvraj <devyuvraj883@gmail.com>
2024-09-24 19:15:32 +0530

    Implemented Clerk Authentication

--- e-commerce/main.js
+++ e-commerce/main.js
@@ -1,4 +1,45 @@
 import { addToCart } from "./scripts/addToCart.js";
+import { Clerk } from "@clerk/clerk-js";
+
+//Authentication using clerk
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const clerk = new Clerk(clerkPubKey);
+await clerk.load({
+  afterSignInUrl: "/",
+  afterSignUpUrl: "/",
+  signInUrl: "/login",
+  signUpUrl: "/signup",
+  navigate: (to) => {
+    window.location.href = to;
+  },
+  polling: true, // enable session polling
+});
+
+if (clerk.user) {
+
+  const userButtonDiv =
+    document.getElementById("user-button");
+    const userButtonMobileDiv =     document.getElementById("user-button-mobile");
+
+  clerk.mountUserButton(userButtonDiv);
+  clerk.mountUserButton(userButtonMobileDiv);
+} else {
+  document.getElementById("app").innerHTML = `
+    <div id="sign-in"></div>
+  `;
+
+  const signInDiv =
+    document.getElementById("sign-in");
+
+  clerk.mountSignIn(signInDiv);
+}
+
+
+
+
+
+//API Calling
 const URL = 'https://dummyjson.com/products/?limit=20';
 
 let products;
