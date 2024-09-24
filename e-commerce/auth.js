de30c2cdb28544a538c7d9ced5d3b2c6651c607a
Yuvraj <devyuvraj883@gmail.com>
2024-09-24 22:27:26 +0530

    Updated path

--- e-commerce/auth.js
+++ e-commerce/auth.js
@@ -1,39 +1,44 @@
-import { Clerk } from './node_module/@clerk/clerk-js/dist/clerk.js';
+import { Clerk } from '@clerk/clerk-js';
 
 
 //Authentication using clerk
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-const clerk = new Clerk(clerkPubKey);
-await clerk.load({
-  afterSignInUrl: "/",
-  afterSignUpUrl: "/",
-  signInUrl: "/login",
-  signUpUrl: "/signup",
-  navigate: (to) => {
-    window.location.href = to;
-  },
-  polling: true, // enable session polling
-});
+const clerkFunction = async()=>{
+  const clerk = new Clerk(clerkPubKey);
+  await clerk.load({
+    afterSignInUrl: "/",
+    afterSignUpUrl: "/",
+    signInUrl: "/login",
+    signUpUrl: "/signup",
+    navigate: (to) => {
+      window.location.href = to;
+    },
+    polling: true, // enable session polling
+  });
 
-if (clerk.user) {
+  if (clerk.user) {
 
-  const userButtonDiv =
-    document.getElementById("user-button");
-    const userButtonMobileDiv =     document.getElementById("user-button-mobile");
+    const userButtonDiv =
+      document.getElementById("user-button");
+      const userButtonMobileDiv =     document.getElementById("user-button-mobile");
 
-  clerk.mountUserButton(userButtonDiv);
-  clerk.mountUserButton(userButtonMobileDiv);
-} else {
-  document.getElementById("app").innerHTML = `
-    <div id="sign-in"></div>
-  `;
+    clerk.mountUserButton(userButtonDiv);
+    clerk.mountUserButton(userButtonMobileDiv);
+  } else {
+    document.getElementById("app").innerHTML = `
+      <div id="sign-in"></div>
+    `;
 
-  const signInDiv =
-    document.getElementById("sign-in");
+    const signInDiv =
+      document.getElementById("sign-in");
 
-  clerk.mountSignIn(signInDiv);
+    clerk.mountSignIn(signInDiv);
+  }
+
 }
+clerkFunction();
+
 
 
 
--- e-commerce/vite.config.js
+++ e-commerce/vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: './', // specify the root directory
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'), // adjust if you have a src folder
-    },
-  },
+  // root: './', // specify the root directory
+  // resolve: {
+  //   alias: {
+  //     // '@': resolve(__dirname, '.'), // adjust if you have a src folder
+  //   },
+  // },
   server: {
     open: true, // automatically open the app in the browser
   },
