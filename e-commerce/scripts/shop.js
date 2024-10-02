6f4e02722cf6199f24b29f487e8127ee7d27d3a7
Yuvraj <devyuvraj883@gmail.com>
2024-10-03 00:17:45 +0530

    Created shop.js

---
+++ e-commerce/scripts/shop.js
@@ -0,0 +1,34 @@
+const url = 'https://dummyjson.com/products/category-list';
+let categoryList=[];
+
+async function fetchCategoryList(){
+
+  try{
+  const res = await fetch(url);
+  categoryList = await res.json();
+   displayCategories();
+
+
+  } catch(error){
+    console.log("Could not fetch categories...", error);
+  }
+}
+fetchCategoryList();
+console.log(categoryList);
+
+
+function displayCategories(){
+  // const template = document.querySelector('#category-template');
+  const categoryFilters = document.querySelector('#category-filters');
+
+  categoryList?.forEach((category)=>{
+    // const tempNode = template.content.cloneNode(true);
+    const newOption = document.createElement('option');
+    newOption.value = category;
+    newOption.textContent = category;
+    newOption.classList.add('capitalize');
+    categoryFilters.appendChild(newOption);
+    console.log(category);
+  })
+}
+
