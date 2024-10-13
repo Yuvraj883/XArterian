031ea3b85e3e818bb28757d09c3d548599904fbb
Yuvraj <devyuvraj883@gmail.com>
2024-10-13 20:27:36 +0530

    Updated the number of products being fetched

--- Backend-E-commerce/utils/fetchProducts.js
+++ Backend-E-commerce/utils/fetchProducts.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-axios.get('https://dummyjson.com/products')
+axios.get('https://dummyjson.com/products/?limit=200')
 .then(async(response) =>{
   const products = response?.data?.products;
   await product.insertMany(products);
