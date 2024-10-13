209190fd131d1f19451527e615ff68696e0dd57c
Yuvraj <devyuvraj883@gmail.com>
2024-10-13 20:09:52 +0530

    Added find by id route

--- Backend-E-commerce/routes/products.js
+++ Backend-E-commerce/routes/products.js
@@ -15,5 +15,20 @@ router.get('/', async (req, res)=>{
   }
 })
 
+router.get('/:id', async(req, res)=>{
+  try{
+  const product = await Product.findOne({id:req.params.id});
+  if(!product){
+    return res.status(404).json({message:`Product with ${req.params.id} doesn't exist!!`});
 
-module.exports = router; 
+  }
+  res.send(product);
+}
+catch(error){
+  console.log(`An error occured during fetching the product with id = ${req.params.id}`, error );
+  res.status(500).json({message:`Server error while fetching the product with id = ${req.params.id}`});
+}
+})
+
+
+module.exports = router;
