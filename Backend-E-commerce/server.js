a6ec063b89d51ac70f681c749ff28cc0a47ddb6c
Yuvraj <devyuvraj883@gmail.com>
2024-10-13 19:46:17 +0530

    Added products route

--- Backend-E-commerce/server.js
+++ Backend-E-commerce/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
+const productsRoute = require('./routes/products');
 const cors = require('cors');
 
 // Load environment variables
@@ -29,6 +30,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productsRoute ); 
 app.get('/', (req, res)=>{
   return res.send("Welcome to my Ecommerce site!!");
 })
